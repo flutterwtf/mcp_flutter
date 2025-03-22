0c54ba83fb48a79c0fb10820247079b400e33ed7
Arenukvern <anton@xsoulspace.dev>
2025-03-22 09:45:23 +0300

    fix: client

--- flutter_mcp_extension/lib/envs.dart
+++ flutter_mcp_extension/lib/envs.dart
@@ -4,7 +4,7 @@ class Envs {
   static const tsRpc = (
     host: String.fromEnvironment('TS_RPC_HOST', defaultValue: 'localhost'),
     port: int.fromEnvironment('TS_RPC_PORT', defaultValue: 3334),
-    path: String.fromEnvironment('TS_RPC_PATH'),
+    path: String.fromEnvironment('TS_RPC_PATH', defaultValue: 'ws'),
   );
   static const flutterRpc = (
     host: String.fromEnvironment('FLUTTER_RPC_HOST', defaultValue: 'localhost'),
