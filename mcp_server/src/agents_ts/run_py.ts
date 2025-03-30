6a782c905321b96c4655b51a52d394a47d100b4f
Arenukvern <anton@xsoulspace.dev>
2025-03-30 03:48:21 +0300

    fix: docker

--- mcp_server/src/agents_ts/run_py.ts
+++ mcp_server/src/agents_ts/run_py.ts
@@ -1,18 +1,18 @@
-import { interpreter as py, PyModule } from "node-calls-python";
+// import { interpreter as py, PyModule } from "node-calls-python";
 
-// https://www.npmjs.com/package/node-calls-python
-class PyAgent {
-  private pymodule: PyModule;
-  constructor() {
-    py.addImportPath("../venv/Lib/site-packages");
+// // https://www.npmjs.com/package/node-calls-python
+// class PyAgent {
+//   private pymodule: PyModule;
+//   constructor() {
+//     py.addImportPath("../venv/Lib/site-packages");
 
-    this.pymodule = py.importSync("", true);
-  }
+//     this.pymodule = py.importSync("", true);
+//   }
 
-  async run(code: string) {
-    const result = await py.eval(this.pymodule, code);
-    return result;
-  }
-}
+//   async run(code: string) {
+//     const result = await py.eval(this.pymodule, code);
+//     return result;
+//   }
+// }
 
-export default PyAgent;
+// export default PyAgent;
